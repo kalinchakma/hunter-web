b5d48d5c04b6f00657a669b08b46ec28a01eb334
Hunter <chakma.kalin10211@gmail.com>
2021-01-29 01:52:08 +0600

    second commit

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
-<?php
-    echo "This is content";
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hunter</title>
+</head>
+<body>
+    <h1>This is page heading</h1>
+</body>
+</html>
